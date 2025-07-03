b6fe7b9fdf91275dcc9f99216feee2550d7f7e43
Eduardo Maia <edumaia909@gmail.com>
2025-07-03 14:13:20 -0300

    test: use in memory databases for register unit tests

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,9 +1,19 @@
 import { UsersRepository } from "@/repositories/users-repository";
-import { User, Prisma } from "@prisma/client";
+import { User, Prisma, Role } from "@prisma/client";
 import { randomUUID } from "node:crypto";
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = [];
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find((item) => item.email === email);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
@@ -12,6 +22,7 @@ export class InMemoryUsersRepository implements UsersRepository {
       email: data.email,
       password_hash: data.password_hash,
       created_at: new Date(),
+      role: Role.MEMBER,
     };
 
     this.items.push(user);
