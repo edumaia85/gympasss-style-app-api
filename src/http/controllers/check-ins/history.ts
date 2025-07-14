1ce86da2301b0938da1d8a78139e53bbd03a850e
Eduardo Maia <edumaia909@gmail.com>
2025-07-14 16:39:25 -0300

    feat: add controllers for check-in

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,22 @@
+import { makeFetchUserCheckInsUseCase } from "@/use-cases/factories/check-in/make-fetch-user-check-ins-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function history(request: FastifyRequest, reply: FastifyReply) {
+  const checkInHistoryQuerySchema = z.object({
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { page } = checkInHistoryQuerySchema.parse(request.query);
+
+  const fetchUserCheckInsUseCase = makeFetchUserCheckInsUseCase();
+
+  const { checkIns } = await fetchUserCheckInsUseCase.execute({
+    userId: request.user.sub,
+    page,
+  });
+
+  return reply.status(200).send({
+    checkIns,
+  });
+}
