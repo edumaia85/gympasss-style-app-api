4fec458021477b612d10a45fc93ae422859f2a7c
Eduardo Maia <edumaia909@gmail.com>
2025-07-14 09:12:39 -0300

    feat: create get user metrics use case

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,7 +4,7 @@ export interface CheckInsRepository {
   // findById(id: string): Promise<CheckIn | null>;
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
-  // countByUserId(userId: string): Promise<number>;
+  countByUserId(userId: string): Promise<number>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
   // save(checkIn: CheckIn): Promise<CheckIn>;
 }
