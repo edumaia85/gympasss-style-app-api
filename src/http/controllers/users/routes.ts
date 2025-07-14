7aa62dabfdf9b9992be632dc725e1891d0dac415
Eduardo Maia <edumaia909@gmail.com>
2025-07-14 14:33:40 -0300

    feat: implement jwt with fastify

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
+import { profile } from "./profile";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  /** Authenticated */
+  app.get("/me", profile);
 }
