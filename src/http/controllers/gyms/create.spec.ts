2da17bcfdaad621d9f80094f16fa3540583bdc0b
Eduardo Maia <edumaia909@gmail.com>
2025-07-14 17:18:52 -0300

    test: add tests for gyms controller

--- src/http/controllers/gyms/create.spec.ts
+++ src/http/controllers/gyms/create.spec.ts
@@ -3,7 +3,7 @@ import { app } from "@/app";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
 
-describe("Profile (e2e)", () => {
+describe("Create Gym (e2e)", () => {
   beforeAll(async () => {
     await app.ready();
   });
@@ -12,19 +12,20 @@ describe("Profile (e2e)", () => {
     await app.close();
   });
 
-  it("should be able to get user profile", async () => {
+  it("should be able to create a gym", async () => {
     const { token } = await createAndAuthenticateUser(app);
 
     const reponse = await request(app.server)
-      .get("/me")
+      .post("/gyms")
       .set("Authorization", `Bearer ${token}`)
-      .send();
+      .send({
+        title: "Unifit",
+        description: "Some description",
+        phone: "(33)9999-8888",
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      });
 
-    expect(reponse.statusCode).toEqual(200);
-    expect(reponse.body.user).toEqual(
-      expect.objectContaining({
-        email: "johndoe@example.com",
-      })
-    );
+    expect(reponse.statusCode).toEqual(201);
   });
 });
