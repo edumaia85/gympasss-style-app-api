13db11270db8f65afacbde69ca31f97d46b96231
Eduardo Maia <160018591+edumaia85@users.noreply.github.com>
2025-07-22 10:48:57 -0300

    Update verify-user-role.ts

--- src/http/middlewares/verify-user-role.ts
+++ src/http/middlewares/verify-user-role.ts
@@ -1,6 +1,6 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 
-export function verifyUserRole(roleToVerify: "ADMIN" | "MEBER") {
+export function verifyUserRole(roleToVerify: "ADMIN" | "MEMBER") {
   return async (request: FastifyRequest, reply: FastifyReply) => {
     const { role } = request.user;
 
